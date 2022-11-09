686b069b3d317c2e46a8128f681cc5e79b7e7233
Courtney Marasigan <coumardev@gmail.com>
2022-11-09 17:43:48 -0500

    thru bonus 5

--- app.js
+++ app.js
@@ -14,12 +14,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then((beersFromApi) => {
+    console.log('Beers from the database: ', beersFromApi)
+
+    res.render('beers', { beersFromApi })
+      // using curly braces turns the array into an object
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then((responseFromApi) => {
+    console.log('Beers from the database: ', responseFromApi)
+
+    res.render('random-beer', {responseFromApi});
+  })
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
